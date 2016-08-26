b6b45227c4b2e23906501aa1389986d1ca9cfa10
n1v0lg <nikolaj@bu.edu>
2016-08-26 16:14:30 -0400

    Implemented Select obl logic

--- src/mpc/obligation.cc
+++ src/mpc/obligation.cc
@@ -18,6 +18,7 @@
 
 #include "mpc/obligation.h"
 #include "ir/value.h"
+#include "ir/condition_tree.h"
 
 namespace musketeer {
 namespace mpc {
@@ -71,17 +72,76 @@ namespace mpc {
     }
 
     bool Obligation::CanPass(SelectOperator* other) {
-        return false;
+        // SelectOp could either be a projection on the columns of the input
+        // or a conditional filter (or both?)
+        if (op->get_output_relation()->get_columns().size() 
+            != other->get_output_relation()->get_columns().size()) {
+            // the columns have changed so we can't push
+            // TODO(nikolaj): do a finer grained analysis here.
+            // there might be cases when it's still okay to push the obligation.
+            return false;
+        }
+        // TODO(nikolaj): check if columns were reordered
+
+        ConditionTree* tree = other->get_condition_tree();
+        CondOperator* cond_op = tree->get_cond_operator();
+
+        if (cond_op) {
+            // gist: not safe to filter on *value* columns 
+            // but safe to filter on *key* columns
+            ConditionTree* left = tree->get_left();
+            ConditionTree* right = tree->get_right();
+
+            if (!(left->isColumn() || right->isColumn() || left->isValue() || right->isValue())) {
+                // Won't deal with multi-level condition trees for now
+                return false;
+            }
+
+            if (left->isColumn() && right->isColumn()) {
+                // both operands are columns, so we can't push
+                return false;
+            }
+            if (left->isValue() && right->isValue()) {
+                // both operands should not be values
+                LOG(ERROR) << "Both operands of condition tree are values";
+                return false;
+            }
+            // at this point either left or right must be a column
+            Column* col = NULL;
+            int index_agg = op->get_columns()[0]->get_index();
+            if (left->isColumn()) {
+                col = left->get_column();
+            }
+            else if (right->isColumn()) {
+                col = right->get_column();
+            }
+            return index_agg == col->get_index();
+        }
+        else {
+            // no conditional operator means we have a singleton value or column
+            Value* val = tree->get_value();
+            if (val && val->get_type() == BOOLEAN_TYPE && val->get_value() == "true") {
+                // safe to push since all rows are retained so the key space does
+                // not change
+                return true;
+            }
+            // conditional is column or false so we can't push 
+            // (the key space might have changed)
+            return false;
+        }
+
+        return true;
     }
 
     bool Obligation::CanPass(MulOperator* other) {
         // sanity check: output sizes must be same
         if (op->get_output_relation()->get_columns().size() 
             != other->get_output_relation()->get_columns().size()) {
-            LOG(ERROR) << "Output sizes of obligation and operator different.";
+            LOG(ERROR) << "Output sizes of obligation and mul operator different.";
             return false;
         }
 
+        // assuming no nested ops, only single product
         vector<Value*> values = other->get_values();
         Value* left = values[0];
         Value* right = values[1];
